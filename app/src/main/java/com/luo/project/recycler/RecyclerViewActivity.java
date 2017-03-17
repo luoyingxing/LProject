bfaa2c174411b30d51acb0348d9e439d42e72f65
luoyingxing <luoyingxing@126.com>
2017-03-17 14:41:12 +0800

    add adapter

--- app/src/main/java/com/luo/project/recycler/RecyclerViewActivity.java
+++ app/src/main/java/com/luo/project/recycler/RecyclerViewActivity.java
@@ -3,8 +3,6 @@ package com.luo.project.recycler;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
@@ -24,8 +22,8 @@ import java.util.List;
 
 public class RecyclerViewActivity extends AppCompatActivity implements View.OnClickListener {
     private Button mAddBtn;
-    private RecyclerView mRecyclerView;
-    private ReAdapter mAdapter;
+    private XRecyclerView mRecyclerView;
+    private XAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +32,17 @@ public class RecyclerViewActivity extends AppCompatActivity implements View.OnCl
 
         mAddBtn = (Button) findViewById(R.id.button_add);
         mAddBtn.setOnClickListener(this);
-        mRecyclerView = (RecyclerView) findViewById(R.id.rv_recycler);
+        mRecyclerView = (XRecyclerView) findViewById(R.id.rv_recycler);
 
-        mAdapter = new ReAdapter(getDataList());
+        mAdapter = new XAdapter<String>(this, getDataList(), R.layout.item_recycler_view) {
+            @Override
+            public void convert(ViewHolder holder, String data) {
+                holder.setText(R.id.text, data);
+            }
+        };
+
         mRecyclerView.setAdapter(mAdapter);
+
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
 
         //TODO 线性布局
@@ -54,68 +59,21 @@ public class RecyclerViewActivity extends AppCompatActivity implements View.OnCl
         mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
 
 
-        mAdapter.setOnItemClickListener(new ReAdapter.OnItemClickListener() {
+        mAdapter.setOnItemClickListener(new XAdapter.OnItemClickListeners<String>() {
             @Override
-            public void onItemClick(View view, int position) {
-                Toast.makeText(getApplicationContext(), mAdapter.getItem(position), Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onItemLongClick(View view, int position) {
-
+            public void onItemClick(RecyclerView.ViewHolder holder, String item, int position) {
+                Toast.makeText(getApplicationContext(), item, Toast.LENGTH_SHORT).show();
             }
         });
 
 
-        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-
+        mRecyclerView.setOnLoadMoreListener(new XRecyclerView.OnLoadMoreListener() {
             @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-                int lastPosition = -1;
-                //当前RecyclerView显示出来的最后一个的item的position
-
-                //当前状态为停止滑动状态SCROLL_STATE_IDLE时
-                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                    RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
-                    if (layoutManager instanceof GridLayoutManager) {
-                        //通过LayoutManager找到当前显示的最后的item的position
-                        lastPosition = ((GridLayoutManager) layoutManager).findLastVisibleItemPosition();
-                    } else if (layoutManager instanceof LinearLayoutManager) {
-                        lastPosition = ((LinearLayoutManager) layoutManager).findLastVisibleItemPosition();
-                    } else if (layoutManager instanceof StaggeredGridLayoutManager) {
-                        //因为StaggeredGridLayoutManager的特殊性可能导致最后显示的item存在多个，所以这里取到的是一个数组
-                        //得到这个数组后再取到数组中position值最大的那个就是最后显示的position值了
-                        int[] lastPositions = new int[((StaggeredGridLayoutManager) layoutManager).getSpanCount()];
-                        ((StaggeredGridLayoutManager) layoutManager).findLastVisibleItemPositions(lastPositions);
-                        lastPosition = findMax(lastPositions);
-                    }
-
-                    //时判断界面显示的最后item的position是否等于itemCount总数-1也就是最后一个item的position
-                    //如果相等则说明已经滑动到最后了
-                    if (lastPosition == recyclerView.getLayoutManager().getItemCount() - 1) {
-                        Toast.makeText(RecyclerViewActivity.this, "滑动到底了", Toast.LENGTH_SHORT).show();
-                    }
-
-                }
-            }
-
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
+            public void onLoadMore() {
+                mAdapter.addAll(getDataList());
             }
         });
-    }
 
-    //找到数组中的最大值
-    private int findMax(int[] lastPositions) {
-        int max = lastPositions[0];
-        for (int value : lastPositions) {
-            if (value > max) {
-                max = value;
-            }
-        }
-        return max;
     }
 
     private int temp = 0;
@@ -123,7 +81,7 @@ public class RecyclerViewActivity extends AppCompatActivity implements View.OnCl
     private List<String> getDataList() {
         List<String> strings = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            strings.add("离离原上草" + temp);
+            strings.add("离离原上草,一岁一枯荣，野火烧不尽，传峰" + temp);
             temp++;
         }
         return strings;
