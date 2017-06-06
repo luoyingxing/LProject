c6664e88f761ec6e8219098364a2c0ee63b04049
luoyingxing <luoyingxing@126.com>
2017-06-06 22:26:01 +0800

    update

--- app/src/main/java/com/luo/project/animator/AnimatorActivity.java
+++ app/src/main/java/com/luo/project/animator/AnimatorActivity.java
@@ -1,11 +1,14 @@
 package com.luo.project.animator;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
@@ -41,9 +44,19 @@ public class AnimatorActivity extends AppCompatActivity {
             }
         });
 
-        verticalRun(imageView);
+//        verticalRun(imageView);
+
+        animationSet(imageViewOne);
     }
 
+    /**
+     * 透明度：alpha
+     * 旋转度数: rotation、rotationX、rotationY
+     * 平移：translationX、translationY
+     * 缩放：scaleX、scaleY
+     *
+     * @param view view
+     */
     public void rotateAnimRun(final View view) {
         ObjectAnimator anim = ObjectAnimator
                 .ofFloat(view, "rotationX", 1.0F, 0.0F)
@@ -78,5 +91,43 @@ public class AnimatorActivity extends AppCompatActivity {
                 view.setTranslationX((Float) animation.getAnimatedValue());
             }
         });
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                ViewGroup parent = (ViewGroup) view.getParent();
+                if (parent != null) {
+                    parent.removeView(view);
+                }
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
     }
+
+    /**
+     * after(Animator anim) 将传入的动画在原有动画执行之前执行：
+     */
+    private void animationSet(View v) {
+        // 将after中传入的缩放动画在原有动画之前执行
+        ObjectAnimator rotateAnim = ObjectAnimator.ofFloat(v, "rotation", 0.0f, 360f);
+        ObjectAnimator scaleAnim = ObjectAnimator.ofFloat(v, "scaleX", 1.0f, 0.0f, 2.0f, 1.0f);
+        AnimatorSet animSet = new AnimatorSet();
+        animSet.play(rotateAnim).with(scaleAnim);
+        animSet.setDuration(2000);
+        animSet.start();
+    }
+
 }
