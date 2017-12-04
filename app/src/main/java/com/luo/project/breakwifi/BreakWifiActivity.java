69766ed9df01e6bca6e85c954cb13d8930c21cff
luoyingxing <luoyingxing@126.com>
2017-12-04 10:42:04 +0800

    update

--- app/src/main/java/com/luo/project/breakwifi/BreakWifiActivity.java
+++ app/src/main/java/com/luo/project/breakwifi/BreakWifiActivity.java
@@ -1,15 +1,267 @@
 package com.luo.project.breakwifi;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.luo.project.R;
+import com.luo.project.adapter.CommonAdapter;
+import com.luo.project.adapter.ViewHolder;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BreakWifiActivity extends AppCompatActivity {
+    private ListView listView;
+    private TextView searchTV;
+
+    private CommonAdapter<ScanResult> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_break_wifi);
+
+        listView = (ListView) findViewById(R.id.list);
+        searchTV = (TextView) findViewById(R.id.search);
+
+        searchTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchWifi();
+            }
+        });
+
+
+        adapter = new CommonAdapter<ScanResult>(this, new ArrayList<ScanResult>(), R.layout.item_search_wifi) {
+            @Override
+            public void convert(ViewHolder helper, ScanResult item) {
+
+                StringBuilder result = new StringBuilder();
+
+                result = result.append("信号强弱:" + item.level + "\n")
+                        .append("名称:" + item.SSID + "\n")
+                        .append("地址:" + item.BSSID + "\n")
+                        .append("加密方案:" + item.capabilities + "\n")
+                        .append("交流访问点:" + item.frequency);
+
+                helper.setText(R.id.tv_item_name, result.toString());
+            }
+        };
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                connect(adapter.getItem(position));
+            }
+        });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initWifi();
+    }
+
+    //定义一个WifiManager对象
+    private WifiManager mWifiManager;
+    //定义一个WifiInfo对象
+    private WifiInfo mWifiInfo;
+    //扫描出的网络连接列表
+    private List<ScanResult> mWifiList;
+    //网络连接列表
+    private List<WifiConfiguration> mWifiConfigurations;
+
+    private void initWifi() {
+        mWifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        //取得WifiInfo对象
+        mWifiInfo = mWifiManager.getConnectionInfo();
+    }
+
+    private void searchWifi() {
+        startScan();
+        analyzeResult();
+    }
+
+    private void startScan() {
+        mWifiManager.startScan();
+        //得到扫描结果
+        mWifiList = mWifiManager.getScanResults();
+        //得到配置好的网络连接
+        mWifiConfigurations = mWifiManager.getConfiguredNetworks();
+    }
+
+    private void analyzeResult() {
+        if (mWifiList != null) {
+            adapter.clear();
+            adapter.addAll(mWifiList);
+        }
+    }
+
+    private void connect(ScanResult result) {
+        Log.e(getClass().getName(), "" + result.SSID);
+
+        //createWifiConfig主要用于构建一个WifiConfiguration，代码中的例子主要用于连接不需要密码的Wifi
+        //WifiManager的addNetwork接口，传入WifiConfiguration后，得到对应的NetworkId
+        int netId = mWifiManager.addNetwork(createWifiConfig(result.SSID, "123456789", WIFICIPHER_NOPASS));
+
+        Log.i(getClass().getName(), "netId: " + netId);
+
+        //WifiManager的enableNetwork接口，就可以连接到netId对应的wifi了
+        //其中boolean参数，主要用于指定是否需要断开其它Wifi网络
+        boolean enable = mWifiManager.enableNetwork(netId, true);
+        Log.e(getClass().getName(), "Enable: " + enable);
+
+        //可选操作，让Wifi重新连接最近使用过的接入点
+        //如果上文的enableNetwork成功，那么reconnect同样连接netId对应的网络
+        //若失败，则连接之前成功过的网络
+        if (!enable) {
+            boolean reconnect = mWifiManager.reconnect();
+            Log.d(getClass().getName(), "reconnect ... " + reconnect);
+        }
+
+    }
+
+    private static final int WIFICIPHER_NOPASS = 0;
+    private static final int WIFICIPHER_WEP = 1;
+    private static final int WIFICIPHER_WPA = 2;
+
+    private WifiConfiguration createWifiConfig(String ssid, String password, int type) {
+        //初始化WifiConfiguration
+        WifiConfiguration config = new WifiConfiguration();
+        config.allowedAuthAlgorithms.clear();
+        config.allowedGroupCiphers.clear();
+        config.allowedKeyManagement.clear();
+        config.allowedPairwiseCiphers.clear();
+        config.allowedProtocols.clear();
+
+        //指定对应的SSID
+        config.SSID = "\"" + ssid + "\"";
+
+        //如果之前有类似的配置
+        WifiConfiguration tempConfig = isExist(ssid);
+        if (tempConfig != null) {
+            //则清除旧有配置
+            mWifiManager.removeNetwork(tempConfig.networkId);
+        }
+
+        //不需要密码的场景
+        if (type == WIFICIPHER_NOPASS) {
+            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+            //以WEP加密的场景
+        } else if (type == WIFICIPHER_WEP) {
+            config.hiddenSSID = true;
+            config.wepKeys[0] = "\"" + password + "\"";
+            config.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+            config.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.SHARED);
+            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+            config.wepTxKeyIndex = 0;
+            //以WPA加密的场景，自己测试时，发现热点以WPA2建立时，同样可以用这种配置连接
+        } else if (type == WIFICIPHER_WPA) {
+            config.preSharedKey = "\"" + password + "\"";
+            config.hiddenSSID = true;
+            config.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+            config.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+            config.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+            config.status = WifiConfiguration.Status.ENABLED;
+        }
+
+        return config;
+    }
+
+    private WifiConfiguration isExist(String ssid) {
+        List<WifiConfiguration> configs = mWifiManager.getConfiguredNetworks();
+        for (WifiConfiguration config : configs) {
+            if (config.SSID.equals("\"" + ssid + "\"")) {
+                return config;
+            }
+        }
+        return null;
+    }
+
+    //通过反射的方式去判断wifi是否已经连接上，并且可以开始传输数据
+    private boolean checkWiFiConnectSuccess() {
+        Class classType = WifiInfo.class;
+        try {
+            Object invo = classType.newInstance();
+            Object result = invo.getClass().getMethod("getMeteredHint").invoke(invo);
+            return (boolean) result;
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            return false;
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            return false;
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        receiver = new Receiver();
+
+        //="android.net.wifi.STATE_CHANGE"  监听wifi状态的变化
+        IntentFilter filter = new IntentFilter(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        registerReceiver(receiver, filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(receiver);
+    }
+
+    private Receiver receiver;
+
+    private class Receiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            if (intent.getAction() != null && intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
+                List<ScanResult> list = mWifiManager.getScanResults();
+                Log.i(getClass().getName(), "扫描数量： " + list.size());
+            }
+
+            ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo wifiInfo = manager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+            if (wifiInfo.isConnected()) {
+                String wifiSSID = mWifiManager.getConnectionInfo().getSSID();
+                Log.i(getClass().getName(), "wifi succeed --> " + wifiSSID);
+
+                Toast.makeText(context, wifiSSID + "连接成功", Toast.LENGTH_SHORT).show();
+            } else {
+                Log.i(getClass().getName(), "wifi failed");
+            }
+        }
+    }
+
 }
